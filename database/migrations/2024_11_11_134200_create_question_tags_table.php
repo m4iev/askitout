542fc1d0b99caa99bf2d77301dc72324d5a6b5e8
Hafidz Ahlaqul Anbiya <anbiya574@gmail.com>
2024-11-11 20:44:05 +0700

    Edit migration users dan tambah migration questions, answers, comments, votes, tags, question_tags

---
+++ database/migrations/2024_11_11_134200_create_question_tags_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('question_tags', function (Blueprint $table) {
+            // $table->foreignIdFor(Question::class);
+            // $table->foreignIdFor(Tag::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('question_tags');
+    }
+};
